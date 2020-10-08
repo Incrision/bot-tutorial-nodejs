c3103fa87d7ca59ae548e36e415f4e3a912c110e
Incrision <62572729+Incrision@users.noreply.github.com>
2020-10-08 16:00:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,27 +5,12 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/Dye #$/; 
-      botRegex2 = /^\/thanks$/;
-      
-      coinflip = /^\/flip coin$/;
-  
-  //checking if user message matchesany keyword 
-  if(request.text && botRegex.test(request.text)) { 
-    this.res.writeHead(200); //starts the header
-    postMessage(1);
-    this.res.end(); //closes the header 
-  } else if(request.text && botRegex2.test(request.text)){
-    this.res.writeHead(200); //starts the header
-    postMessage(2);
-    this.res.end(); //closes the header 
-  } else if(request.text && coinflip.test(request.text)){
-    this.res.writeHead(200); //starts the header
-    //if((Math.floor(Math.random() * Math.floor(2)) == 0);
-      //postMessage(66);
-    //else
-      postMessage(67);
-    this.res.end(); //closes the header 
+      botRegex = /^\/cool guy$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
@@ -33,21 +18,11 @@ function respond() {
   }
 }
 
-function postMessage(txt) {
+function postMessage() {
   var botResponse, options, body, botReq;
-  
-  
-  if (txt == 1)
-    botResponse = 'Dye: 7024974714 \n\n*Beep Boop*';
-  else if(txt == 2)
-    botResponse = cool();
-  else if(txt == 66)
-    botResponse = 'heads';
-  else if(txt == 67)
-    botResponse = 'tails';
-  else
-     botResponse = 'fail';
-  
+
+  botResponse = cool();
+
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
@@ -58,6 +33,7 @@ function postMessage(txt) {
     "bot_id" : botID,
     "text" : botResponse
   };
+
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
@@ -79,4 +55,4 @@ function postMessage(txt) {
 }
 
 
-exports.respond = respond; //listener
+exports.respond = respond;
